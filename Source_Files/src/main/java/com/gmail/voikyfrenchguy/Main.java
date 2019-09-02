ac44d0413342d7ea1860be5e23b7a6d6ce10c65f
Yoann <yoann.pommier.777@gmail.com>
2019-09-02 21:06:08 +0200

    nope

--- Source_Files/src/main/java/com/gmail/voikyfrenchguy/Main.java
+++ Source_Files/src/main/java/com/gmail/voikyfrenchguy/Main.java
@@ -2,19 +2,32 @@ package com.gmail.voikyfrenchguy;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public class Main extends JavaPlugin {
+
+    HttpListener httpListener;
+
     @Override
-    public void onLoad() {
-        super.onLoad();
+    public void onEnable() {
+        super.onEnable();
+        try {
+            this.setupAndCheckConfig();
+        } catch (Exception e) {
+            this.getLogger().log(Level.SEVERE, "Config file error: please check your configuration");
+        }
+        this.httpListener = new HttpListener(this.getConfig().getInt("http_port"), this.getConfig().getString("http_auth_key"));
+    }
+
+    private void setupAndCheckConfig() throws Exception {
+        this.saveDefaultConfig();
+        if (!this.getConfig().contains("http_port") || !this.getConfig().contains("http_auth_key")) {
+            throw new Exception();
+        }
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
-    }
-
-    @Override
-    public void onEnable() {
-        super.onEnable();
     }
 }
