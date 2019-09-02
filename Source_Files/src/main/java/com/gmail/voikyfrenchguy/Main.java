a9211547a6d9570abb4249b7030a4a315b9e055a
yoann-shopopop <yoann@shopopop.com>
2019-09-02 14:11:54 +0200

    First Commit

---
+++ Source_Files/src/main/java/com/gmail/voikyfrenchguy/Main.java
@@ -0,0 +1,20 @@
+package com.gmail.voikyfrenchguy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
